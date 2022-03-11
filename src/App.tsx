53bd39675e2afc5f3e960c4bda2e34491938377e
Nicolás Pérez <nicolas014.p@gmail.com>
2022-03-11 18:37:50 -0300

    Se instala correctamente react con template typescript y creo contador a modo test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export function App() {
+
+  const [count, setCount] = useState(1);
+
+  const countFunction = (num: number) =>{
+    setCount(count + num)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <header >
+        <h1>Hola mundo</h1>
+        <p>Primera app</p>
+        <p>Cambio numero 2</p>
+        <h3>Contador: { count }</h3>
+        <button
+          onClick={()=>countFunction(1)}
         >
-          Learn React
-        </a>
+          +1</button>
+
+        <button
+          onClick={()=>countFunction(-1)}
+        >
+          -1</button>
       </header>
-    </div>
+    </>
   );
 }
-
-export default App;
